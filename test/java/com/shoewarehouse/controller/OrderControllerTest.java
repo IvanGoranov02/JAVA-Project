d5879613e33ff742e9cff35f0ce64eb22d82ed62
Ivan Goranov <prostogoranov@gmail.com>
2026-01-16 22:55:45 +0200

    Add AutoConfigureMockMvc to controller tests to disable security filters. Update BrandControllerTest, CategoryControllerTest, OrderControllerTest, ShoeControllerTest, and SupplierControllerTest for improved test configuration.

--- test/java/com/shoewarehouse/controller/OrderControllerTest.java
+++ test/java/com/shoewarehouse/controller/OrderControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OrderController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class OrderControllerTest {
 
     @Autowired
